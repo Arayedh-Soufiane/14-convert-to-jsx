af4167fba7a514ea8d9872ecf984c25dd75089c2
soufiane <arayedh.soufiane@gmail.com>
2020-07-03 11:44:26 +0100

    14 check points convert to jsx

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Imgsrc from "./imageInSrc.jpg";
+import "./App.css";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Your name here</h1>
+        <br />
+        <img src={Imgsrc} /> <br />
+        <img src="/imageInPublic.jpg" />
+      </div>
+
+      <video width="320" height="240" controls>
+        <source src="./myVideo.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+export default App;
